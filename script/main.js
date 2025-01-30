e42e8fc45fc55d6e6f7d08e2d6ebb29f72b2eb87
Тимур обернибесов <obertim@yandex.ru>
2025-01-30 13:16:26 +0300

    add top menu

---
+++ script/main.js
@@ -0,0 +1,16 @@
+const link = document.getElementById('link')
+const pop = document.getElementById('pop')
+
+link.addEventListener('click', function () {
+    console.log('status')
+    pop.classList.toggle('visible')
+})
+
+document.getElementById('burger').addEventListener('click', function() {
+    const nav = document.getElementById('nav');
+    const buttons = document.getElementById('buttons');
+
+    nav.classList.toggle('active');
+    buttons.classList.toggle('active');
+});
+
